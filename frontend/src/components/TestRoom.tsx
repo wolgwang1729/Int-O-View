bb13b2369dd381722f8a8fdfae8a57b857a64e8a
wolgwang <145632038+wolgwang1729@users.noreply.github.com>
2025-03-21 08:52:54 +0530

    No code changes made

--- frontend/src/components/TestRoom.tsx
+++ frontend/src/components/TestRoom.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useRef, useState } from 'react';
-import axios from 'axios';
-import { IconRiCameraLine } from './IconRiCameraLine';
-import { IconRiCameraOffLine } from './IconRiCameraOffline.tsx';
-import { IconRiMicLine } from './IconRiMicLine';
-import { IconRiMicOffLine } from './IconRiMicOffline.tsx';
-import { TextToSpeech } from 'elevenlabs-node';
-import { useNavigate } from 'react-router-dom';
-import logo from '/finalLogo.jpg'
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { IconRiCameraLine } from "./IconRiCameraLine";
+import { IconRiCameraOffLine } from "./IconRiCameraOffline.tsx";
+import { IconRiMicLine } from "./IconRiMicLine";
+import { IconRiMicOffLine } from "./IconRiMicOffline.tsx";
+import { TextToSpeech } from "elevenlabs-node";
+import { useNavigate } from "react-router-dom";
+import logo from "/finalLogo.jpg";
 
 declare global {
   interface Window {
-    SpeechRecognition:any,
+    SpeechRecognition: any;
     webkitSpeechRecognition: any;
   }
 }
 
 function TestRoom() {
   const [isListening, setIsListening] = useState(false);
-  const [transcript, setTranscript] = useState('');
-  const [response, setResponse] = useState('');
+  const [transcript, setTranscript] = useState("");
+  const [response, setResponse] = useState("");
   const [conversationHistory, setConversationHistory] = useState<any[]>([]);
   const [isCameraOn, setIsCameraOn] = useState(false);
   const [isGifVisible, setIsGifVisible] = useState(false);
@@ -57,87 +57,85 @@ function TestRoom() {
 
   // useEffect(() => {
   //   const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-  
+
   //   if (!SpeechRecognition) {
   //     alert("Speech recognition not supported in this browser.");
   //     return;
   //   }
-  
+
   //   const recognition = new SpeechRecognition(); // Use SpeechRecognition here
   //   recogRef.current = recognition;
-  
+
   //   recognition.lang = 'en-US';
   //   recognition.interimResults = false;
   //   recognition.maxAlternatives = 1;
-  
+
   //   recognition.onstart = () => {
   //     console.log("Recognition started");
   //   };
-  
+
   //   recognition.onresult = (e:any) => {
   //     const transcriptResult = e.results[0][0].transcript;
   //     console.log("Transcript: ", transcriptResult);
   //     setTranscript(transcriptResult);
   //   };
-  
+
   //   recognition.onspeechend = () => {
   //     console.log("Speech ended");
   //     setIsListening(false);
   //     recognition.stop();
   //   };
-  
+
   //   recognition.onerror = (e:any) => {
   //     console.error("Error during recognition: ", e.error);
   //     setIsListening(false);
   //     recognition.stop();
   //   };
-  
+
   //   recognition.onend = () => {
   //     console.log("Recognition stopped");
   //   };
   // }, []);
 
   useEffect(() => {
-    if (!('webkitSpeechRecognition' in window)) {
+    if (!("webkitSpeechRecognition" in window)) {
       alert("Speech recognition not supported.");
       return;
     }
-  
+
     const recognition = new window.webkitSpeechRecognition();
     recogRef.current = recognition;
-  
-    recognition.lang = 'en-US';
+
+    recognition.lang = "en-US";
     recognition.interimResults = false;
     recognition.maxAlternatives = 1;
-  
+
     recognition.onstart = () => {
       console.log("Recognition started");
     };
-  
-    recognition.onresult = (e:any) => {
+
+    recognition.onresult = (e: any) => {
       const transcriptResult = e.results[0][0].transcript;
       console.log("Transcript: ", transcriptResult);
       setTranscript(transcriptResult);
     };
-  
+
     recognition.onspeechend = () => {
       console.log("Speech ended");
       setIsListening(false);
       recognition.stop();
     };
-  
-    recognition.onerror = (e:any) => {
+
+    recognition.onerror = (e: any) => {
       console.error("Error during recognition: ", e.error);
       setIsListening(false);
       recognition.stop();
     };
-  
+
     recognition.onend = () => {
       console.log("Recognition stopped");
     };
   }, []);
-  
-  
 
   const startListening = () => {
     if (isListening) {
@@ -152,90 +150,97 @@ function TestRoom() {
   const handleEndInterview = async () => {
     try {
       // Send the `exit` query via a POST request
-      await axios.post('http://localhost:3000/api/v1/user/callModel', { query: 'exit' });
-      
+      await axios.post("http://localhost:3000/api/v1/user/callModel", {
+        query: "exit",
+      });
+
       // After the API call, navigate to the dashboard
-      navigate('/dashboard');
+      navigate("/dashboard");
     } catch (error) {
-      console.error('Error ending interview:', error);
+      console.error("Error ending interview:", error);
       // Handle error as needed (e.g., show an error message)
     }
   };
 
   let currentAudio: HTMLAudioElement | null = null;
 
-const speakText = async (newResponse: string) => {
-  if (newResponse) {
-    console.log("great");
-    try {
-      console.log(newResponse);
-      const apiKey = 'sk_733ba366b5f566c3097fa2a4866d96fe0bb2952845fd4abb';
-      const url = 'https://api.elevenlabs.io/v1/text-to-speech/H6QPv2pQZDcGqLwDTIJQ';
+  const speakText = async (newResponse: string) => {
+    if (newResponse) {
+      console.log("great");
+      try {
+        console.log(newResponse);
+        const apiKey = "sk_733ba366b5f566c3097fa2a4866d96fe0bb2952845fd4abb";
+        const url =
+          "https://api.elevenlabs.io/v1/text-to-speech/H6QPv2pQZDcGqLwDTIJQ";
 
-      const options = {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          'xi-api-key': apiKey,
-        },
-        body: JSON.stringify({
-          text: newResponse,
-          voice_settings: {
-            stability: 0.5,
-            similarity_boost: 0.5,
+        const options = {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            "xi-api-key": apiKey,
           },
-        }),
-      };
+          body: JSON.stringify({
+            text: newResponse,
+            voice_settings: {
+              stability: 0.5,
+              similarity_boost: 0.5,
+            },
+          }),
+        };
 
-      const res = await fetch(url, options);
+        const res = await fetch(url, options);
 
-      if (!res.ok) {
-        throw new Error('Failed to fetch the audio');
+        if (!res.ok) {
+          throw new Error("Failed to fetch the audio");
+        }
+
+        // Stop the previous audio if it is still playing
+        if (currentAudio) {
+          currentAudio.pause();
+          currentAudio.currentTime = 0; // Reset the audio
+        }
+
+        const audioUrl = URL.createObjectURL(await res.blob());
+
+        // Create new audio and assign it to currentAudio
+        currentAudio = new Audio(audioUrl);
+        currentAudio.play();
+
+        setIsGifVisible(true);
+        currentAudio.onended = () => {
+          setIsGifVisible(false);
+          currentAudio = null; // Clear the reference when done
+        };
+      } catch (error) {
+        console.error("Error speaking text:", error);
       }
-
-      // Stop the previous audio if it is still playing
-      if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0; // Reset the audio
-      }
-
-      const audioUrl = URL.createObjectURL(await res.blob());
-
-      // Create new audio and assign it to currentAudio
-      currentAudio = new Audio(audioUrl);
-      currentAudio.play();
-
-      setIsGifVisible(true);
-      currentAudio.onended = () => {
-        setIsGifVisible(false);
-        currentAudio = null; // Clear the reference when done
-      };
-
-    } catch (error) {
-      console.error('Error speaking text:', error);
     }
-  }
-};
-
+  };
 
   useEffect(() => {
     speakText(response);
   }, [response]);
-  
+
   useEffect(() => {
     speakText();
-  },[response])
+  }, [response]);
 
   const getResponse = async () => {
     try {
-      const res = await axios.post('http://localhost:3000/api/v1/user/callModel', { query: transcript });
+      const res = await axios.post(
+        "http://localhost:3000/api/v1/user/callModel",
+        { query: transcript }
+      );
       const newResponse = res.data.message;
       setResponse(newResponse);
-      setConversationHistory([...conversationHistory, { user: transcript, ai: newResponse }]);
+      setConversationHistory([
+        ...conversationHistory,
+        { user: transcript, ai: newResponse },
+      ]);
       // speakText();
     } catch (error) {
-      console.error('Error fetching response:', error);
+      console.error("Error fetching response:", error);
     }
   };
 
@@ -254,13 +259,15 @@ const speakText = async (newResponse: string) => {
       setIsCameraOn(false);
     } else {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
         setIsCameraOn(true);
       } catch (error) {
-        console.error('Error accessing camera:', error);
+        console.error("Error accessing camera:", error);
       }
     }
   };
@@ -269,11 +276,7 @@ const speakText = async (newResponse: string) => {
     <div className="flex flex-col min-h-screen font-sans text-gray-200 bg-zinc-900">
       <div className="flex justify-between p-4 bg-zinc-800">
         <h2 className="text-2xl font-semibold">
-          <img
-          src='logo2_cropped.jpg'
-          alt='logo'
-          className='h-12 rounded-md'
-          />
+          <img src="logo2_cropped.jpg" alt="logo" className="h-12 rounded-md" />
         </h2>
         <button
           onClick={handleEndInterview}
@@ -289,7 +292,11 @@ const speakText = async (newResponse: string) => {
           <div className="flex flex-row justify-center w-full gap-4">
             {/* Camera Area */}
             <div className="relative w-1/2 max-w-sm overflow-hidden bg-gray-800 h-72 rounded-3xl">
-              <video ref={videoRef} autoPlay className={`w-full h-full ${isCameraOn ? 'block' : 'hidden'}`}></video>
+              <video
+                ref={videoRef}
+                autoPlay
+                className={`w-full h-full ${isCameraOn ? "block" : "hidden"}`}
+              ></video>
               {!isCameraOn && (
                 <div className="flex items-center justify-center h-full text-3xl text-white">
                   User
@@ -299,7 +306,7 @@ const speakText = async (newResponse: string) => {
 
             {/* GIF Area */}
             <div className="relative items-center justify-center w-1/2 max-w-sm overflow-hidden bg-black h-72 rounded-3xl">
-              {(isGifVisible && !isListening) ? (
+              {isGifVisible && !isListening ? (
                 <img
                   src="interview_room.gif"
                   alt="Interviewer"
@@ -310,7 +317,6 @@ const speakText = async (newResponse: string) => {
                   src="interview_room_1_crop.jpg"
                   alt="Interviewer"
                   className="object-cover w-full h-full"
-
                 />
               )}
             </div>
@@ -320,7 +326,9 @@ const speakText = async (newResponse: string) => {
           <div className="flex justify-center p-2 rounded-3xl">
             <button
               onClick={toggleCamera}
-              className={`items-center justify-center p-4 mr-4 rounded-full ${isCameraOn ? 'bg-green-600' : 'bg-red-600'}`}
+              className={`items-center justify-center p-4 mr-4 rounded-full ${
+                isCameraOn ? "bg-green-600" : "bg-red-600"
+              }`}
             >
               {isCameraOn ? (
                 <IconRiCameraLine className="text-3xl" />
@@ -331,9 +339,15 @@ const speakText = async (newResponse: string) => {
 
             <button
               onClick={startListening}
-              className={`items-center justify-center p-4 rounded-full ${isListening ? 'bg-green-600' : 'bg-red-600'}`}
+              className={`items-center justify-center p-4 rounded-full ${
+                isListening ? "bg-green-600" : "bg-red-600"
+              }`}
             >
-              {isListening ? <IconRiMicLine className="text-3xl" /> : <IconRiMicOffLine className="text-3xl" />}
+              {isListening ? (
+                <IconRiMicLine className="text-3xl" />
+              ) : (
+                <IconRiMicOffLine className="text-3xl" />
+              )}
             </button>
           </div>
 
@@ -346,7 +360,8 @@ const speakText = async (newResponse: string) => {
             )}
             {response && (
               <p className="p-4 mt-2 text-lg rounded-md">
-                <span className="font-semibold">AI Response:</span> <br /> {response}
+                <span className="font-semibold">AI Response:</span> <br />{" "}
+                {response}
               </p>
             )}
           </div>
@@ -369,7 +384,13 @@ const speakText = async (newResponse: string) => {
   );
 }
 
-function CollapsibleMessage({ message, isUser }: { message: string, isUser?: boolean }) {
+function CollapsibleMessage({
+  message,
+  isUser,
+}: {
+  message: string;
+  isUser?: boolean;
+}) {
   const [isExpanded, setIsExpanded] = useState(false);
   const MAX_LENGTH = 40; // Maximum length before collapsing
 
@@ -379,12 +400,12 @@ function CollapsibleMessage({ message, isUser }: { message: string, isUser?: boo
 
   return (
     <p className="cursor-pointer" onClick={toggleExpand}>
-      <span className="font-semibold">{isUser ? 'User:' : 'AI Response:'}</span>{' '}
+      <span className="font-semibold">{isUser ? "User:" : "AI Response:"}</span>{" "}
       {isExpanded || message.length <= MAX_LENGTH ? (
         <span>{message}</span>
       ) : (
         <span>
-          {message.substring(0, MAX_LENGTH)}...{' '}
+          {message.substring(0, MAX_LENGTH)}...{" "}
           <button className="text-blue-500">Expand</button>
         </span>
       )}
